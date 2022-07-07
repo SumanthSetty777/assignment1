b6cd22e9327be9ac31c4509a6bce47bc3176f771
SumanthSetty <51389658+SumanthSetty777@users.noreply.github.com>
2022-07-08 00:03:30 +0530

    Update new_branch.sh

--- new_branch.sh
+++ new_branch.sh
@@ -14,7 +14,9 @@ do
     git checkout -b "$release_name"
     echo "Created a branch successfully"
     
-    git push https://SumanthSetty777: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@github.com/SumanthSetty777/"$repo".git "$release_name"
+    //git push https://SumanthSetty777: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@github.com/SumanthSetty777/"$repo".git "$release_name"
+    withCredentials([usernamePassword(credentialsId: '5a32f9c5-aee6-4368-83b9-98dbd8af656f', passwordVariable: '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', usernameVariable: 'SumanthSetty777')]) { sh('git push https://SumanthSetty777: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@github.com/SumanthSetty777/vedx.git new3')}
+                  
     echo "Pushed Successfully"
     
 done
