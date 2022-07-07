d8e4758481e2a34a89d7810f8e82b41a8c0ad357
SumanthSetty <51389658+SumanthSetty777@users.noreply.github.com>
2022-07-08 02:08:55 +0530

    Update new_branch.sh

--- new_branch.sh
+++ new_branch.sh
@@ -5,7 +5,7 @@ release_name=test3
 arr=(assignment1 vedx)
 
 for repo in "${arr[@]}"
-do
+
     git clone https://SumanthSetty777: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@github.com/SumanthSetty777/"$repo".git "$HOME/Documents/newbranch/$repo"
     echo "Cloned Succesfully"
 
@@ -14,7 +14,7 @@ do
     git checkout -b "$release_name"
     echo "Created a branch successfully"
     
-    git config credential.helper store
+    #git config credential.helper store
     git push https://SumanthSetty777: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@github.com/SumanthSetty777/"$repo".git "$release_name"
                   
     echo "Pushed Successfully"
