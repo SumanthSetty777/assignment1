d9bfcf70f6a34fec2dd327a80c7003ae24fc29f4
SumanthSetty <51389658+SumanthSetty777@users.noreply.github.com>
2022-07-08 23:00:37 +0530

    Update new_branch.sh

--- new_branch.sh
+++ new_branch.sh
@@ -15,7 +15,7 @@ for repo in "${arr[@]}"
     echo "Created a branch successfully"
     
     #git config credential.helper store
-    git push https://SumanthSetty777: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@github.com/SumanthSetty777/"$repo".git "$release_name"
+    git push https://SumanthSetty777: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@github.com/SumanthSetty777/"$repo".git "$release_name"
                   
     echo "Pushed Successfully"
     
