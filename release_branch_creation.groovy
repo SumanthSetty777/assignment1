8a44fffe4083c550078d70d8763b8decc358911d
SumanthSetty <51389658+SumanthSetty777@users.noreply.github.com>
2022-07-12 01:53:05 +0530

    Update release_branch_creation.groovy

--- release_branch_creation.groovy
+++ release_branch_creation.groovy
@@ -5,12 +5,12 @@ pipeline{
             steps{
               
               script{
-                  def arr = ["r4e-ops-test", "r4e-demo-tenant"]
+                  def arr = ["assignment1", "vedx"]
                   
                   for(repo in arr){
                     
                         echo "Testing the $repo browser"
-                        sh "git clone https://sumanthsetty78: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@github.com/reputation/${repo}.git"
+                        sh "git clone https://github.com/reputation/${repo}.git"
                         sh "cd $WORKSPACE/$repo && git checkout -b Test"    
                         sh("cd $WORKSPACE/$repo && git push origin Test")   
                         
